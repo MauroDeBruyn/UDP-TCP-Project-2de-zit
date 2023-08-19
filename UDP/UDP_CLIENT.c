6f72089bf86aa48af05d31bedc66d3f973608803
Mauro <maurodebruyn@hotmail.com>
2023-08-19 15:39:52 +0200

    Upload clean perror code client

--- UDP/UDP_CLIENT.c
+++ UDP/UDP_CLIENT.c
@@ -1,12 +1,11 @@
 #ifdef _WIN32
-	#define _WIN32_WINNT _WIN32_WINNT_WIN7 //select minimal legacy support, needed for inet_pton, inet_ntop
+	#define _WIN32_WINNT _WIN32_WINNT_WIN7
 	#include <winsock2.h> //for all socket programming
 	#include <ws2tcpip.h> //for getaddrinfo, inet_pton, inet_ntop
-	#include <stdio.h> //for fprintf
+	#include <stdio.h> //for fprintf, perror
 	#include <unistd.h> //for close
 	#include <stdlib.h> //for exit
 	#include <string.h> //for memset
-	#define clrscr() printf("\e[1;1H\e[2J")
 	void OSInit( void )
 	{
 		WSADATA wsaData;
@@ -22,7 +21,6 @@
 		WSACleanup();
 	}
 	#define perror(string) fprintf( stderr, string ": WSA errno = %d\n", WSAGetLastError() )
-
 #else
 	#include <sys/socket.h> //for sockaddr, socket, socket
 	#include <sys/types.h> //for size_t
@@ -34,10 +32,8 @@
 	#include <unistd.h> //for close
 	#include <stdlib.h> //for exit
 	#include <string.h> //for memset
-	#define clrscr() printf("\e[1;1H\e[2J")
-	void OSInit(void){}
-	void OSCleanup(void){}
-
+	void OSInit( void ) {}
+	void OSCleanup( void ) {}
 #endif
 
 int initialization( struct sockaddr ** internet_address, socklen_t * internet_address_length );
@@ -62,6 +58,7 @@ int main( int argc, char * argv[] )
 
 	execution( internet_socket, internet_address, internet_address_length );
 
+
 	////////////
 	//Clean up//
 	////////////
@@ -81,41 +78,29 @@ int initialization( struct sockaddr ** internet_address, socklen_t * internet_ad
 	memset( &internet_address_setup, 0, sizeof internet_address_setup );
 	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_DGRAM;
-	int getaddrinfo_return = getaddrinfo( "127.0.0.1", "24044", &internet_address_setup, &internet_address_result );
+	int getaddrinfo_return = getaddrinfo( "::1", "24042", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
 		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
 		exit( 1 );
 	}
 
-	int internet_socket = -1;
-	struct addrinfo * internet_address_result_iterator = internet_address_result;
-	while( internet_address_result_iterator != NULL )
-	{
-		//Step 1.2
-		internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
-		if( internet_socket == -1 )
-		{
-			perror( "socket" );
-		}
-		else
-		{
-			//Step 1.3
-			*internet_address_length = internet_address_result_iterator->ai_addrlen;
-			*internet_address = (struct sockaddr *) malloc( internet_address_result_iterator->ai_addrlen );
-			memcpy( *internet_address, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
-			break;
-		}
-		internet_address_result_iterator = internet_address_result_iterator->ai_next;
-	}
-
-	freeaddrinfo( internet_address_result );
-
+	//Step 1.2
+	int internet_socket;
+	internet_socket = socket( internet_address_result->ai_family, internet_address_result->ai_socktype, internet_address_result->ai_protocol );
 	if( internet_socket == -1 )
 	{
-		fprintf( stderr, "socket: no valid socket address found\n" );
+		perror( "socket" );
+		freeaddrinfo( internet_address_result );
 		exit( 2 );
 	}
+
+	//Step 1.3
+	*internet_address_length = internet_address_result->ai_addrlen;
+	*internet_address = (struct sockaddr *) malloc( internet_address_result->ai_addrlen );
+	memcpy( *internet_address, internet_address_result->ai_addr, internet_address_result->ai_addrlen );
+
+	freeaddrinfo( internet_address_result );
 
 	return internet_socket;
 }
@@ -124,103 +109,25 @@ void execution( int internet_socket, struct sockaddr * internet_address, socklen
 {
 	//Step 2.1
 	int number_of_bytes_send = 0;
-	number_of_bytes_send = sendto( internet_socket, "GO", 2, 0, internet_address, internet_address_length );
+	number_of_bytes_send = sendto( internet_socket, "Hello UDP world!", 16, 0, internet_address, internet_address_length );
 	if( number_of_bytes_send == -1 )
 	{
 		perror( "sendto" );
 	}
 
 	//Step 2.2
-	char largest_int_received[1000];
 	int number_of_bytes_received = 0;
 	char buffer[1000];
-	int count = 0;
-
-	while(count < 9)
-	{
-		number_of_bytes_received = recvfrom( internet_socket, buffer, ( sizeof buffer ) - 1, 0, internet_address, &internet_address_length );
-
-		if( number_of_bytes_received == -1 )
-		{
-			perror( "recvfrom" );
-		}
-
-		else
-		{
-			buffer[number_of_bytes_received] = '\0';
-		}
-
-		if(buffer < largest_int_received)
-		{
-			strcpy(largest_int_received, buffer);
-		}
-
-		else
-		{}
-
-		clrscr();
-		printf("\rLargest int received: %s", largest_int_received);
-
-		count++;
-	}
-
-	//Step 2.3
-	number_of_bytes_send = sendto( internet_socket, largest_int_received, 16, 0, internet_address, internet_address_length );
-	if( number_of_bytes_send == -1 )
-	{
-		number_of_bytes_send = sendto( internet_socket, largest_int_received, 16, 0, internet_address, internet_address_length );
-	}
-
-	//Step 2.4
-	char largest_int_received_2[1000];
-	int number_of_bytes_received_2 = 0;
-	char buffer_2[1000];
-	int count_2 = 0;
-
-	while(count_2 < 9)
-	{
-		number_of_bytes_received_2 = recvfrom( internet_socket, buffer_2, ( sizeof buffer_2 ) - 1, 0, internet_address, &internet_address_length );
-
-		if( number_of_bytes_received_2 == -1 )
-		{
-			perror( "recvfrom" );
-		}
-
-		else
-		{
-			buffer_2[number_of_bytes_received_2] = '\0';
-		}
-
-		if(buffer_2 < largest_int_received_2)
-		{
-			strcpy(largest_int_received_2, buffer_2);
-		}
-
-		else
-		{}
-
-		clrscr();
-		printf("\rLargest int received: %s", largest_int_received_2);
-
-		count_2++;
-	}
-
-	//Step 2.5
-	number_of_bytes_send = sendto( internet_socket, largest_int_received_2, 16, 0, internet_address, internet_address_length );
-	if( number_of_bytes_send == -1 )
-	{
-		number_of_bytes_send = sendto( internet_socket, largest_int_received_2, 16, 0, internet_address, internet_address_length );
-	}
-
-	char respons[3];
-
-	number_of_bytes_received_2 = recvfrom( internet_socket, respons, ( sizeof respons ) - 1, 0, internet_address, &internet_address_length );
-
-	if( number_of_bytes_received_2 == -1 )
+	number_of_bytes_received = recvfrom( internet_socket, buffer, ( sizeof buffer ) - 1, 0, internet_address, &internet_address_length );
+	if( number_of_bytes_received == -1 )
 	{
 		perror( "recvfrom" );
 	}
-	printf("\nServer respons: %s", respons);
+	else
+	{
+		buffer[number_of_bytes_received] = '\0';
+		printf( "Received : %s\n", buffer );
+	}
 }
 
 void cleanup( int internet_socket, struct sockaddr * internet_address )
